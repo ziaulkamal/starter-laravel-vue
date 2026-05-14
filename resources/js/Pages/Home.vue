6021b603418fcf5c768bf4b4630a507f36b4c4b8
Ziaul Kamal <ziaul.kamal1109@gmail.com>
2026-05-14 17:03:04 +0700

    init inertia setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Laravel + Vue + Inertia</h1>
+        <p>Setup berhasil!</p>
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
+        vue(),
     ],
     server: {
         watch: {
