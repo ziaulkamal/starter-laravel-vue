88e8ddbe348c847da864662bdf5906fcd143b34b
Ziaul Kamal <ziaul.kamal1109@gmail.com>
2026-05-15 18:38:57 +0700

    table component

---
+++ resources/js/Components/UI/index.ts
@@ -0,0 +1,6 @@
+export { default as AppTable } from './AppTable.vue';
+export type { TableColumn, TableActions } from './AppTable.vue';
+export { default as IconPicker } from './IconPicker.vue';
+
+// Form components
+export * from './Form';
