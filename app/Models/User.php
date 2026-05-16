c0e8401ca18ffccc0d8923730e8af9671308c2eb
Ziaul Kamal <ziaul.kamal1109@gmail.com>
2026-05-16 10:17:39 +0700

    include phone fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password', 'avatar', 'is_active', 'last_login_at'])]
+#[Fillable(['name', 'email', 'phone', 'password', 'avatar', 'is_active', 'last_login_at'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
